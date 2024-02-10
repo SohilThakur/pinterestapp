2407ef74122946c9b8338a74f696827bc9186869
SohilThakur <sohilthakur0011@gmail.com>
2024-02-11 01:20:13 +0530

    Login and Signup

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,29 @@ const postModel = require("./posts");
 const passport = require("passport");
 
 const localStrategy = require("passport-local");
-passport.authenticate(new localStrategy(userModel.authenticate()));
+passport.use(new localStrategy(userModel.authenticate()));
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
 router.get("/profile",isLoggedIn, function (req, res, next) {
   res.send("profile");
 });
 
-router.post("/register", function (req, res) {
-  console.log(req);
+router.post("/register",function (req, res) {
   const { username, email, fullname } = req.body;
   const userData = new userModel({ username, email, fullname });
-
-  userModel.register(userData, req, body.password).then(function () {
+  userModel.register(userData, req.body.password).then(function () {
     passport.authenticate("local")(req, res, function () {
       res.redirect("/profile");
-    });
+    })
   });
 });
 
